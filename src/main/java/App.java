7b300e3db82325498760a1ee9e8143c7d514de8c
cngondo <devngondo@gmail.com>
2017-01-18 14:14:27 +0300

    loading .vtl from the App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
 import static spark.Spark.*;
 
 public class App {
@@ -7,42 +11,18 @@ public class App {
 		//Port Number
 		port(1234);
 		//Hello route
-     get("/", (request, response) -> 
-     	 "<!DOCTYPE html>" +
-         "<html>" +
-         "<head>" +
-           "<title>Hello Friend!</title>" +
-           "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-         "</head>" +
-        "<body>" +
-           "<h1>Hello From Afar</h1>" +
-           "<p>Dear Friend,</p>" +
-           "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
-           "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
-           "<p>But I like programming a lot, so I've got that going for me. </p>" +
-           "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
-           "<p>Cheers,</p>" +
-           "<p>Travel Enthusiast Jane</p>" +
-         "</body>" +
-       "</html>"
-     	);
+		/*
+			Make sure that the get request returns the required velocity template
+		*/
+     get("/", (request, response) -> {
+     	  return new ModelAndView(new HashMap(), "templates/hello.vtl");
+		     }, new VelocityTemplateEngine()
+	    );
      // Favourite photos route
      get("/favourite_photos", (request, response) ->
-     	 "<!DOCTYPE html>" +
-      "<html>" +
-      "<head>" +
-        "<title>Hello Friend!</title>" +
-        "<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-      "</head>" +
-      "<body>" +
-       "<h1>Favorite Traveling Photos</h1>" +
-          "<ul>" +
-            "<li><img src='/images/foggymountain.jpeg' alt='A photo of a mountain.'/></li>"
-            + "<br>" +
-            "<li><img src='/images/rockycoast.jpeg' alt='A photo of a a rocky beach.'/></li>" +
-          "</ul>" +
-      "</body>" +
-      "</html>"
+     	 {
+     	 	return new ModelAndView(new HashMap(), "templates/favorite_photos.vtl");
+     	 }, new VelocityTemplateEngine()
      	);
   }
 }
